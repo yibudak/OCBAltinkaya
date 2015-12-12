f0f046d28a8881b3ab874f5a74d38ce1b0679d0e
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2015-12-12 20:44:20 +0200

    Added two shell script for fast update and start

---
+++ odooupdate.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./openerp-server -d upgrade1 -u all --stop-after-init --config=/etc/odoo-server.conf
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo -u odoo /opt/odoo/odoo-server/openerp-server --config=/etc/odoo-server.conf
