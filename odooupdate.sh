c6da5c54d0ab139b064fc71aab134401f9dee8c3
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2015-12-27 22:49:52 +0200

    Changed database update script

--- odooupdate.sh
+++ odooupdate.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
-./openerp-server -d upgrade1 -u all --stop-after-init --config=/etc/odoo-server.conf
+#!/bin/bash
+read -e -p "\n Enter odoo database name to update: " ODOODB
+./openerp-server -d $ODOODB -u all --stop-after-init --config=/etc/odoo-server.conf
