26f128815c8124fc9d7f8d4ba7f3cb80cb91a789
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2016-01-02 16:06:32 +0200

    removed sent by company using text

--- addons/mail/mail_followers.py
+++ addons/mail/mail_followers.py
@@ -155,12 +155,12 @@ class mail_notification(osv.Model):
             company = "<a style='color:inherit' href='%s'>%s</a>" % (website_url, user.company_id.name)
         else:
             company = user.company_id.name
-        sent_by = _('Sent by %(company)s using %(odoo)s')
-
-        signature_company = '<br /><small>%s</small>' % (sent_by % {
-            'company': company,
-            'odoo': "<a style='color:inherit' href='https://www.odoo.com/'>Odoo</a>"
-        })
+        sent_by = _('')
+        signature_company =''
+#        signature_company = '<br /><small>%s</small>' % (sent_by % {
+#            'company': company,
+#            'odoo': "<a style='color:inherit' href='https://www.odoo.com/'>Odoo</a>"
+#        })
         footer = tools.append_content_to_html(footer, signature_company, plaintext=False, container_tag='div')
 
         return footer
