68b12da1131f1536466d77200d254cc3270a2fa3
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2018-12-05 07:58:09 +0300

    update module script update

--- updateModule.sh
+++ updateModule.sh
@@ -5,20 +5,18 @@ if [ "$(id -u)" = "0" ]; then
    exit 1
 fi
 
-read -e -p "Enter odoo database name to update: " ODOODB
-echo "You should update module listing before this step"
-read -e -p "Enter comma separated name of modules to update:" MODULES
-
 while true; do
     read -p "Would you like to update modules, odoo server will stop during process  (y/n)?" yn
     case $yn in
         [Yy]* ) 
-        sudo /etc/init.d/odoo-server stop
-        sudo service odoo stop
+	echo -e "stopping odoo server"
+        sudo systemctl stop odoo
         echo -e "Updating Modules"
-        ./openerp-server -d $ODOODB -u $MODULES --stop-after-init --config=/etc/odoo-server.conf --workers=0 --max-cron-threads=0
-        sudo /etc/init.d/odoo-server start
-        sudo service odoo start
+        cd /opt/odoo/odoo-server
+        /opt/odoo/odoo-server/openerp-server -d $1 -u $2 --stop-after-init --config=/etc/odoo-server.conf --workers=0 --max-cron-threads=0
+	sudo tail  /var/log/odoo/odoo-server.log
+        echo *e "Starting odoo server"
+	sudo systemctl start odoo
         break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
