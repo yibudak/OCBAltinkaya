d0e9109bf9687930d53415eb94d4336b9fa3098a
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2022-04-01 20:46:35 +0300

    added partner display name to the validation user error messages

--- addons/account/wizard/account_invoice_state.py
+++ addons/account/wizard/account_invoice_state.py
@@ -16,7 +16,8 @@ class AccountInvoiceConfirm(models.TransientModel):
         context = dict(self._context or {})
         active_ids = context.get('active_ids', []) or []
 
-        invoice_list = self.env['account.invoice'].browse(active_ids).sorted(lambda i: (i.date_invoice or fields.Date.context_today(self), i.reference or '', i.id))
+        invoice_list = self.env['account.invoice'].browse(active_ids).sorted(
+            lambda i: (i.date_invoice or fields.Date.context_today(self), i.reference or '', i.id))
         for record in invoice_list:
             if record.state != 'draft':
                 raise UserError(_("Selected invoice(s) cannot be confirmed as they are not in 'Draft' state."))
