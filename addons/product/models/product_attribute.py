073e63f33463111196e5541bef47caaa373b87e9
yiğit budak <yibudak@gmail.com>
2023-06-24 15:57:48 +0300

    [16.0][FIX] Import errors

--- addons/product/models/product_attribute.py
+++ addons/product/models/product_attribute.py
@@ -265,41 +265,41 @@ class ProductTemplateAttributeLine(models.Model):
         res._update_product_template_attribute_values()
         return res
 
-    def write(self, values):
-        """Override to:
-        - Add constraints to prevent doing changes that are not supported such
-            as modifying the template or the attribute of existing lines.
-        - Clean up related values and related variants when archiving or when
-            updating `value_ids`.
-        """
-        if 'product_tmpl_id' in values:
-            for ptal in self:
-                if ptal.product_tmpl_id.id != values['product_tmpl_id']:
-                    raise UserError(
-                        _("You cannot move the attribute %s from the product %s to the product %s.") %
-                        (ptal.attribute_id.display_name, ptal.product_tmpl_id.display_name, values['product_tmpl_id'])
-                    )
-
-        if 'attribute_id' in values:
-            for ptal in self:
-                if ptal.attribute_id.id != values['attribute_id']:
-                    raise UserError(
-                        _("On the product %s you cannot transform the attribute %s into the attribute %s.") %
-                        (ptal.product_tmpl_id.display_name, ptal.attribute_id.display_name, values['attribute_id'])
-                    )
-        # Remove all values while archiving to make sure the line is clean if it
-        # is ever activated again.
-        if not values.get('active', True):
-            values['value_ids'] = [(5, 0, 0)]
-        res = super(ProductTemplateAttributeLine, self).write(values)
-        if 'active' in values:
-            self.env.flush_all()
-            self.env['product.template'].invalidate_model(['attribute_line_ids'])
-        # If coming from `create`, no need to update the values and the variants
-        # before all lines are created.
-        if self.env.context.get('update_product_template_attribute_values', True):
-            self._update_product_template_attribute_values()
-        return res
+    # def write(self, values):
+    #     """Override to:
+    #     - Add constraints to prevent doing changes that are not supported such
+    #         as modifying the template or the attribute of existing lines.
+    #     - Clean up related values and related variants when archiving or when
+    #         updating `value_ids`.
+    #     """
+    #     if 'product_tmpl_id' in values:
+    #         for ptal in self:
+    #             if ptal.product_tmpl_id.id != values['product_tmpl_id']:
+    #                 raise UserError(
+    #                     _("You cannot move the attribute %s from the product %s to the product %s.") %
+    #                     (ptal.attribute_id.display_name, ptal.product_tmpl_id.display_name, values['product_tmpl_id'])
+    #                 )
+    #
+    #     if 'attribute_id' in values:
+    #         for ptal in self:
+    #             if ptal.attribute_id.id != values['attribute_id']:
+    #                 raise UserError(
+    #                     _("On the product %s you cannot transform the attribute %s into the attribute %s.") %
+    #                     (ptal.product_tmpl_id.display_name, ptal.attribute_id.display_name, values['attribute_id'])
+    #                 )
+    #     # Remove all values while archiving to make sure the line is clean if it
+    #     # is ever activated again.
+    #     if not values.get('active', True):
+    #         values['value_ids'] = [(5, 0, 0)]
+    #     res = super(ProductTemplateAttributeLine, self).write(values)
+    #     if 'active' in values:
+    #         self.env.flush_all()
+    #         self.env['product.template'].invalidate_model(['attribute_line_ids'])
+    #     # If coming from `create`, no need to update the values and the variants
+    #     # before all lines are created.
+    #     if self.env.context.get('update_product_template_attribute_values', True):
+    #         self._update_product_template_attribute_values()
+    #     return res
 
     def unlink(self):
         """Override to:
